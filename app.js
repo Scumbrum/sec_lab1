69821ca55342055c9022d05a402cd4e672e97a72
ItSites <front3@itsites.com.ua>
2023-06-06 16:11:34 +0300

    inti

---
+++ app.js
@@ -0,0 +1,69 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import fs from 'fs/promises';
+
+const ROLES = {
+  admin: 'admin',
+  user: 'user',
+};
+
+const users = [
+  {
+    name: 'vlad',
+    password: '123123',
+    role: ROLES.admin,
+  },
+  {
+    name: 'dumpUser',
+    password: '123123',
+    role: ROLES.user,
+  },
+];
+
+const rl = readline.createInterface({ input, output });
+
+const name = await rl.question('Name: ');
+
+const password = await rl.question('Password: ');
+
+const user = users.find(
+  user => user.name === name && user.password === password,
+);
+
+if (user) {
+  console.log(`Hello ${user.name}`);
+
+  const storage = await rl.question('Chose disk: ');
+
+  const disks = ['A', 'B', 'C'];
+
+  if (disks.includes(storage)) {
+    const action = await rl.question('Chose action: ');
+
+    switch (action) {
+      case 'read':
+        console.log(await fs.readdir(storage));
+        break;
+
+      case 'create':
+        if (user.role === ROLES.admin) {
+          const file = await rl.question('Write file name: ');
+          const data = await rl.question('Write file data: ');
+
+          await fs.writeFile(storage + '/' + file, data);
+          break;
+        }
+        console.warn('You are not admin !');
+        break;
+
+      default:
+        console.warn('You are debil 2');
+    }
+  } else {
+    console.warn('You are debil');
+  }
+} else {
+  console.log('Login error');
+}
+
+rl.close();
